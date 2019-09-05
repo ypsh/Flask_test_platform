0c7320a5eb996956e658fac1decb97d0d343a656
ypsh <qianmengtmc@163.com>
2019-09-05 11:50:29 +0800

    增加接口场景测试功能，持续修改中

--- com/service/runjob.py
+++ com/service/runjob.py
@@ -54,7 +54,7 @@ class Run_job:
         #         if str(line).find("runjob") != -1:
         #             dataSet.append(line)
         dataSet = self.r.lrange("logs", 0, 1000)
-        return {"logs": dataSet[0:40][::-1], "run_status": self.r.get("run_status")}
+        return {"logs": dataSet[0:25][::-1], "run_status": self.r.get("run_status")}
 
     def run(self, to_date, project_code="xy"):
         try:
